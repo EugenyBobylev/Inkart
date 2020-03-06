e84a24dd802ef9ebca956ae7b0bc0dc8791c13b4
Bobylev <ebobylev@gmailcom>
2020-03-06 12:39:07 +1000

    фигня

--- app/timeloopTest.py
+++ app/timeloopTest.py
@@ -5,21 +5,25 @@ from datetime import timedelta
 tl = Timeloop()
 
 
-#@tl.job(interval=timedelta(seconds=2))
+@tl.job(interval=timedelta(seconds=2))
 def sample_job_every_2s():
     print("2s job current time : {}".format(time.ctime()))
 
 
 @tl.job(interval=timedelta(seconds=5))
 def sample_job_every_5s():
-    time.sleep(10)
     print("5s job current time : {}".format(time.ctime()))
 
 
-#@tl.job(interval=timedelta(seconds=10))
+@tl.job(interval=timedelta(seconds=10))
 def sample_job_every_10s():
     print("10s job current time : {}".format(time.ctime()))
 
 
 if __name__ == "__main__":
-    tl.start(block=True)
+    tl.start()
+    while True:
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            tl.stop()
