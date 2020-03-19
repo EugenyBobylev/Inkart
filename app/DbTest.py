99583cef6dd88c21a9980b0645d3c4a1cfc2ffe9
Bobylev <ebobylev@gmailcom>
2020-03-19 13:35:38 +1000

    DbTest  - тест потокобезопасной работы с БД в SqlAlchemy Проверка передачи экземпляра объекта между сессиями

--- app/DbTest.py
+++ app/DbTest.py
@@ -16,25 +16,33 @@ def print_job(job_id: str):
             print("job is None")
 
 
-def add_job(id: str) -> None:
+def print_job_v2(job: IncartJob):
+    with dal.session_scope() as session:
+        job.candidate_id = 12345
+        job.doctor_id = 12345
+        session.add(job)
+        print(f'{job.id=}; {job.snippet=}; {job.candidate_id=}; {job.doctor_id=}')
+
+
+def add_job(id: str) -> IncartJob:
     job: IncartJob = IncartJob()
     job.id = id
     job.snippet = 'test job'
     dal.session.add(job)
     dal.session.commit()
-    print(f'{job.id=}; {job.snippet=}; {job.candidate_id=}; {job.doctor_id=}')
     dal.session.close()
     print(f'{job.id=}; {job.snippet=}; {job.candidate_id=}; {job.doctor_id=}')
     print('job added successful')
+    return job
 
 
 if __name__ == "__main__":
     dal.connect()
-    add_job('9876543210')
+    job = add_job('9876543210')
 
     #job_id = '987654321'
-    #t: Thread = Thread(target=print_job, args=(job_id,))
-    #t.start()
-    #t.join()
+    t: Thread = Thread(target=print_job_v2, args=(job,))
+    t.start()
+    t.join()
     print("exit")
 
