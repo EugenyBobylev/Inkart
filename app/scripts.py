48ebf8c5aef86f945a1946b5e39168b03923e64d
Eugeny Bobylev <ebobylev@gmail.com>
2020-07-27 11:49:21 +0300

    cheked functionality scripts.py

--- app/scripts.py
+++ app/scripts.py
@@ -1,4 +1,8 @@
 from app.GMailApi import get_service, get_all_unread_emails, modify_message
+import app.WhatsappChanel
+
+
+BOBYLEV = 96881373
 
 
 def set_mail_unread():
@@ -42,5 +46,39 @@ def check_gmail():
     print(f'count of unread emails = {cnt}')
 
 
+def get_all_whatsapp_clients():
+    clients = app.WhatsappChanel.get_api_all_clients()
+    cnt = len(clients['data'])
+    print_dict(clients['data'])
+
+
+def get_whatsapp_bobylev():
+    bobylev = app.WhatsappChanel.get_api_clients(BOBYLEV)
+    print(bobylev['data'])
+
+
+def send_whatsapp_message(message: str):
+    app.WhatsappChanel.post_api_message(BOBYLEV, message)
+
+
+def get_whatsapp_client_messages(client_id=BOBYLEV):
+    """Messages sent by client"""
+    messages_info = app.WhatsappChanel.get_api_messages(BOBYLEV)
+    return messages_info['data']
+
+
+def check_whatsapp():
+    """Check functionality whatsapp api"""
+    # send_whatsapp_message('сообещние отправлено через chat2desk')
+    messages = get_whatsapp_client_messages()
+    print_dict(messages)
+
+
+def print_dict(dct):
+    for item in dct:
+        print(item)
+
+
 if __name__ == '__main__':
-    check_gmail()
+    # check_gmail()
+    check_whatsapp()
